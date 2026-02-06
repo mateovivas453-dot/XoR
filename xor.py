cd92d00f487e8da1e7415b2af402f0e798921985
mateovivas453-dot <mateovivas453@gmail.com>
2026-02-06 16:53:19 -0500

    Add files via upload

---
+++ xor.py
@@ -0,0 +1,24 @@
+def cifrado_xor(texto, clave):
+
+    if isinstance(texto, str):
+        texto = texto.encode()
+
+    if isinstance(clave, str):
+        clave = clave.encode()
+
+    if len(clave) < len(texto):
+        clave = clave * (len(texto) // len(clave) + 1)
+        clave = clave[:len(texto)]
+
+  
+    return bytes([t ^ c for t, c in zip(texto, clave)])
+
+
+texto_original = "Hola Mundo!"
+llave = b'secreto'
+
+mensaje_cifrado = cifrado_xor(texto_original, llave)
+print("Cifrado:", mensaje_cifrado)
+
+mensaje_descifrado = cifrado_xor(mensaje_cifrado, llave)
+print("Descifrado:", mensaje_descifrado.decode())
